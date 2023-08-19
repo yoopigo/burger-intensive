6ecea59bead2a21fe2d917d5eb0249d42816c0b5
yoopigo <collerggggggg@gmail.com>
2023-08-19 20:38:35 +0500

    day5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  //Тайтл
+
   title = 'burger-intensive';
+  //Скролл кнопок
+
+  scrollTo(target: HTMLElement) {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
 }
